91f2a92587ae2ba58f2667d179e0473bfa00c06c
Sundar  Deshmukh <109438264+Sundiiii@users.noreply.github.com>
2023-09-15 18:18:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ const inn = document.querySelector("input");
 let a=document.getElementById("letterCount");
 inn.addEventListener("input", updateValue);
 
-function updateValue(e) {
-console.log( e.target.value.length);
-	
+function updateValue(e) {	
   a.textContent = e.target.value.length;
 }
