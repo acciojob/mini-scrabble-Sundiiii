fc10a2040a4f428ff286555f8cbc0a78a6b911a2
Sundar  Deshmukh <109438264+Sundiiii@users.noreply.github.com>
2023-09-15 18:16:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-let inn=document.getElementById("in").style.color="red";
-inn.addEventListener("onchange",(x){
-x.value	
-console.log(x.value);
-})
+const inn = document.querySelector("input");
+let a=document.getElementById("letterCount");
+inn.addEventListener("input", updateValue);
+
+function updateValue(e) {
+console.log( e.target.value.length);
+	
+  a.textContent = e.target.value.length;
+}
